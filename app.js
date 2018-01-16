096618b17514ecabb54c3092b7bc95654f018b0d
Tyler Hill <tylerhillwebdev@gmail.com>
2018-01-15 20:23:44 -0800

    JQU

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -20,6 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- public/scrip.js
+++ public/scrip.js
@@ -1,8 +1,2 @@
-//function timeout() {
-//	setTimeout(function(){
-//		document.getElementById('scrolla').removeChild(document.getElementById('scrolla').children[0]);
-//		timeout();
-//	},1000);
-//}
-//
-//timeout();
+$(document).ready(function(){
+});
