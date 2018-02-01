533d2e5f75ba920442349fda27690fc6a8b3a077
TheDripper <tylerehill@gmail.com>
2018-01-31 17:20:33 -0800

    saved your stupid dumb

--- routes/gettit.js
+++ routes/gettit.js
@@ -5,12 +5,22 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 router.get('/',function(req,res,next){
+	const dom = new JSDOM('<!DOCTYPE html>');
 	gettit.getSub('pics').then(function(response,err){
 		gettit.parseSub(response).then(function(response,err){
-			for(i=0;i<response.length;i++) {
-				console.log(response[i]);
+			var tags = [];
+			console.log(dom);
+			for(var i=0;i<response.length;i++) {
+				var tent = response[i].content[0]._;
+				var html = dom.window.document.createElement('div');
+				html.innerHTML = tent.trim();
+				var imgs = html.getElementsByTagName('img');
+				for(var z=0;z<imgs.length;z++) {
+					console.log(imgs[z].src);
+					tags.push(imgs[z].src);
+				}
 			}
-			res.render('gets',{"entry":response});
+			res.render('gets',{"entry":tags});
 		});
 	});
 });
