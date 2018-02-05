988e2d6378d919950e6fb027920118f1ca6ebf00
TheDripper <tylerehill@gmail.com>
2018-02-05 08:19:44 -0800

    leaving

--- routes/gettit.js
+++ routes/gettit.js
@@ -1,25 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var gettit = require('gettit');
+var axios = require('axios');
+var throttle = require('lodash.throttle');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+				//var html = dom.window.document.createElement('div');
+				//html.innerHTML = tent.trim();
+				//var imgs = html.getElementsByTagName('img');
 
 router.get('/',function(req,res,next){
 	const dom = new JSDOM('<!DOCTYPE html>');
 	gettit.getSub('pics').then(function(response,err){
 		gettit.parseSub(response).then(function(response,err){
 			var tags = [];
-			console.log(dom);
-			for(var i=0;i<response.length;i++) {
-				var tent = response[i].content[0]._;
-				var html = dom.window.document.createElement('div');
-				html.innerHTML = tent.trim();
-				var imgs = html.getElementsByTagName('img');
-				for(var z=0;z<imgs.length;z++) {
-					console.log(imgs[z].src);
-					tags.push(imgs[z].src);
-				}
+			var roses = [];
+			var po = response.feed.entry;
+			for(var i=0; i<po.length; i++) {
+				//var link = po[i].link[0].$.href;
 			}
+			//throw new Error('fuck');
+			Promise.all(roses).then(function(values){
+				console.log(values);
+			}).catch(function(err){
+				console.log(err);
+			});
 			res.render('gets',{"entry":tags});
 		});
 	});
